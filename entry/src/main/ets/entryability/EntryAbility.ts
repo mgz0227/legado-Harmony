4bb2b5cf3941680499cfbdd831996dcf3b025235
miaogongzi <miaogongzi0227@gmail.com>
2024-04-25 19:46:00 +0800

    移动icon

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -14,7 +14,14 @@ export default class EntryAbility extends UIAbility {
   onWindowStageCreate(windowStage: window.WindowStage) {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
-
+    // 全屏
+    windowStage.getMainWindow((err, windowClass) => {
+      if (err.code) {
+        hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+        return
+      }
+      windowClass.setWindowLayoutFullScreen(true)
+    })
     windowStage.loadContent('pages/welcomePage', (err, data) => {
       if (err.code) {
         hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
