9421676aa102338ee12221ac0aa8ec61e8898269
gongzi miao <miaogongzi0227@gmail.com>
2025-05-09 19:53:23 +0800

    停止开源

--- entry/web/src/pages/source/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import App from "@/App.vue";
-import sourceRouter from "@/router";
-import store from "@/store";
-
-createApp(App).use(store).use(sourceRouter).mount("#app");
